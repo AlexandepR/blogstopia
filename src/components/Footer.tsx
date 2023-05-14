c416b4da3a2e3516c0372ea0140e2d48c24c0ed1
Alex <alexandepp@gmail.com>
2023-05-14 13:17:38 +0300

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="main_container_footer">
+            <select className="input_post_choose">
+                <option>Show more</option>
+            </select>
+        </div>
+    );
+};
+
+export default Footer;
