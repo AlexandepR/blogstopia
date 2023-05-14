559cb73d553d5466d1367ce80a5acee5814f4eca
Alex <alexandepp@gmail.com>
2023-05-14 13:17:08 +0300

    add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header-wrapper'>
+        {/*<header className='header'>*/}
+            <h1>Blogger Platform</h1>
+        {/*</header>*/}
+        </div>
+    );
+};
+
+export default Header;
