0012c2b593b882feaa557ffb23fb0b7797b9119f
Alex <alexandepp@gmail.com>
2023-05-14 13:17:24 +0300

    add nav

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './SideBar.css';
+import { Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router';
+import Blogs from '../pages/Blogs';
+
+const SideBar = () => {
+    return (
+        // <div className="sideBar">
+        <div>
+            <div className="sideBar-container">
+                <nav>
+                    <ul className="sideBar-list">
+                        <li className="blogs"><Link to="/blogs">Blogs</Link></li>
+                        <li className="posts"><Link to="/posts">Posts</Link></li>
+                    </ul>
+                </nav>
+                {/*<Routes>*/}
+                {/*    <Route path='/blogs' element = {<Blogs />} />*/}
+                {/*</Routes>*/}
+            </div>
+        </div>
+    );
+};
+
+export default SideBar;
