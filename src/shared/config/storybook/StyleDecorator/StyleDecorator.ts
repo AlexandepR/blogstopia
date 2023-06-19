af14ec84a6793b6edf870637d9c11a88f551d5dc
Alex <alexandepp@gmail.com>
2023-06-19 21:50:28 +0300

    decorators for storybook

---
+++ src/shared/config/storybook/StyleDecorator/StyleDecorator.ts
@@ -0,0 +1,10 @@
+import 'app/styles/index.scss';
+import { StoryFn } from '@storybook/react';
+
+export const StyleDecorator = (story: () => StoryFn) => story();
+
+
+// import 'app/styles/index.scss';
+// import { StoryFn } from '@storybook/react';
+//
+// export const StyleDecorator = (story: StoryFn) => story({});
