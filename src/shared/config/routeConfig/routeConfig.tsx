6ae9c6ce32de393c7bee74056a8cb7375facc9f5
Alex <alexandepp@gmail.com>
2023-09-09 12:39:56 +0300

    add route config

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -3,12 +3,14 @@ import { HomePage } from 'src/pages/MainPage';
 import { BlogsPage } from 'src/pages/Blogs';
 import { PostsPage } from 'src/pages/Posts';
 import { NotFoundPage } from 'src/pages/NotFound';
+import TestPage from 'src/pages/Test/ui/TestPage';
 
 
 export enum AppRoutes {
     HOME = 'home',
     BLOGS = 'blogs',
     POSTS = 'posts',
+    TEST_PAGE = 'test',
     NOT_FOUND = 'not_found'
 }
 
@@ -16,6 +18,7 @@ export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.HOME]: '/',
     [AppRoutes.BLOGS]: '/blogs',
     [AppRoutes.POSTS]: '/posts',
+    [AppRoutes.TEST_PAGE]: '/test',
     [AppRoutes.NOT_FOUND]: '*',
 }
 
@@ -31,6 +34,10 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
     [AppRoutes.POSTS]: {
         path: RoutePath.posts,
         element: <PostsPage />
+    },
+    [AppRoutes.TEST_PAGE]: {
+        path: RoutePath.test,
+        element: <TestPage />
     },
     [AppRoutes.NOT_FOUND]: {
         path: RoutePath.not_found,
