96a253b8a7288cecc7c9d1212d3b2e0b99dd481f
Alex <alexandepp@gmail.com>
2023-09-11 18:05:04 +0300

    add render withTranslation

---
+++ src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+import { render } from '@testing-library/react';
+import { I18nextProvider } from 'react-i18next';
+import i18nForTests from 'src/shared/config/i18n/i18nForTests';
+import { MemoryRouter } from 'react-router';
+
+export function renderWithTranslation(component: ReactNode) {
+    return render(
+        <MemoryRouter>                              //context for rout
+            <I18nextProvider i18n={i18nForTests}>
+                {component}
+            </I18nextProvider>
+        </MemoryRouter>
+    );
+}
