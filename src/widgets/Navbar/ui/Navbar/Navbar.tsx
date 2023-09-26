c6f2758343f175cb5d72b3487e4831ad5d6f2104
Alex <alexandepp@gmail.com>
2023-09-26 12:00:47 +0300

    change navbar

--- src/widgets/Navbar/ui/Navbar/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar/Navbar.tsx
@@ -1,25 +1,44 @@
-import { classNames } from 'src/shared/lib/classNames/className';
-import cls from 'src/widgets/Navbar/ui/Navbar/Navbar.module.scss'
-import { useState } from 'react';
-import AppLink, { AppLinkTheme } from 'src/shared/ui/AppLink/AppLink';
+import { classNames } from 'src/shared/lib/classNames/classNames';
+import cls from 'src/widgets/Navbar/ui/Navbar/Navbar.module.scss';
+import { Button, ButtonTheme } from 'src/shared/ui/Button/Button';
+import { memo, useCallback, useState } from 'react';
+import { LoginModal } from 'src/features/AuthByUsername';
+import { useTranslation } from 'react-i18next';
+import { Modal } from 'src/shared/ui/Modal/Modal';
 
-interface SidebarProps {
-    className?: string
+interface NavbarProps {
+    className?: string;
 }
 
-export const Navbar = ({className}: SidebarProps) => {
+export const Navbar = memo(({ className }: NavbarProps) => {
+    const { t } = useTranslation();
+    const [isAuthModal, setIsAuthModal] = useState(false);
+
+    const onShowModal = useCallback(() => {
+        setIsAuthModal((prev) => !prev);
+    }, []);
+
+    const onCloseModal = useCallback(() => {
+        setIsAuthModal(false);
+    }, []);
+
     return (
-        <div className={classNames (cls.navbar, {}, [className || ''])}>
-            <div className={cls.links}>
-                <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>
-                    Главная
-                </AppLink>
-                <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>
-                    О сайте
-                </AppLink>
-            </div>
+        <div className={classNames(cls.navbar, {}, [className])}>
+            <Button
+                theme={ButtonTheme.CLEAR}
+                className={cls.links}
+                onClick={onShowModal}
+            >
+                Login
+            </Button>
+             <Modal
+                isOpen={isAuthModal}
+                onClose={onShowModal}
+            >
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias dignissimos dolor et eveniet harum id ipsam libero magnam maxime modi nihil numquam qui quo quod recusandae rerum saepe, suscipit voluptates.
+            </Modal>
         </div>
     );
-};
+});
 
 export default Navbar;
