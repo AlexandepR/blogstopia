6b65141ecca3ff9448631bf72bf05b4bf5ecb5e6
Alex <alexandepp@gmail.com>
2023-09-27 21:53:25 +0300

    add theme providers

--- src/app/providers/ThemeProviders/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProviders/ui/ThemeProvider.tsx
@@ -3,8 +3,18 @@ import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from '../lib/ThemeContext'
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
 
-export const ThemeProvider: FC<{ children: React.ReactNode }> = ({ children })=> {
-    const [theme, setTheme] = useState<Theme>(defaultTheme);
+interface ThemeProviderProps {
+    initialTheme?: Theme;
+    children: React.ReactNode
+}
+
+const ThemeProvider: FC<ThemeProviderProps> = (props)=> {
+    const {
+        initialTheme,
+        children,
+    } = props;
+
+    const [theme, setTheme] = useState<Theme>(initialTheme || defaultTheme);
 
     const defaultProps = useMemo(() => ({
         theme: theme,
@@ -17,5 +27,4 @@ export const ThemeProvider: FC<{ children: React.ReactNode }> = ({ children })=>
         </ThemeContext.Provider>
     );
 };
-
 export default ThemeProvider;
