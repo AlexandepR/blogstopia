823126e0229f13e117927b5843827264c0f17e13
Alex <alexandepp@gmail.com>
2024-02-06 22:27:18 +0300

    change import THemeSwitcher

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,11 +1,8 @@
-import { classNames } from 'src/shared/lib/classNames/className';
-import cls from 'src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.module.scss';
-// import nightIcon  from 'src/shared/assets/icons/nightIcon.svg'
-// import { ReactComponent as NightIcon } from 'src/shared/assets/icons/nightIcon.svg'
-// import { ReactComponent as NightIcon } from '!!react-svg-loader!src/shared/assets/icons/nightIcon.svg'
-import SvgToReactComponent from 'src/shared/assets/icons/svgToReactComponent';
-import { Theme, useTheme } from 'src/app/providers/ThemeProviders';
-import Button, { ThemeButton } from 'src/shared/ui/Button/Button';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from 'shared/ui/ThemeSwitcher/ui/ThemeSwitcher.module.scss';
+import SvgToReactComponent from 'shared/assets/icons/svgToReactComponent';
+import { Theme, useTheme } from 'app/providers/ThemeProviders';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 
 
 interface ThemeSwitcherProps {
@@ -16,8 +13,8 @@ export const ThemeSwitcher = ({className}: ThemeSwitcherProps) => {
     const { theme, toggleTheme } = useTheme();
     return (
        <Button
-           theme={ThemeButton.CLEAR}
-           className={classNames(cls.ThemeSwitcher,{},[className || ''])}
+           theme={ButtonTheme.CLEAR}
+           className={classNames(cls.ThemeSwitcher!,{},[className])}
            onClick={toggleTheme}>
            {theme === Theme.DARK ? <SvgToReactComponent id='lightIcon'/> : <SvgToReactComponent id='nightIcon'/>}
        </Button>
