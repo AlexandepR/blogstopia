4d3df15f74a8b9f6fbd916d577752c3174d55786
Alex <alexandepp@gmail.com>
2024-02-10 20:05:37 +0300

    change import in errorPage

--- src/widgets/ErrorPage/ui/ErrorPage.tsx
+++ src/widgets/ErrorPage/ui/ErrorPage.tsx
@@ -1,10 +1,7 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-// import { classNames } from 'src/shared/lib/classNames/classNames';
 import cls from 'widgets/ErrorPage/ui/ErrorPage.module.scss';
-// import cls from 'src/ErrorPage.module.scss';
 import { useTranslation } from 'react-i18next';
 import { Button } from 'shared/ui/Button/Button';
-// import { Button } from 'src/shared/ui/Button/Button';
 
 interface ErrorPageProps {
     className?: string
