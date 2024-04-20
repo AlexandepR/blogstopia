2c0778a0100b06c1758ca8e42fd47962160587ee
Alex <alexandepp@gmail.com>
2024-04-20 22:27:46 +0300

    add animation for LangSwitcher

--- src/shared/ui/LangSwitcher/LangSwitcher.tsx
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -1,15 +1,21 @@
 import { classNames } from 'shared/lib/classNames/className';
 import cls from 'shared/ui/LangSwitcher/LangSwitcher.module.scss';
 import { useTranslation } from 'react-i18next';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 
 interface LangSwitcherProps {
     className?: string
+    short: boolean
 }
 
-export const LangSwitcher = ({ className }: LangSwitcherProps) => {
+export const LangSwitcher = ({ className, short }: LangSwitcherProps) => {
     const { t, i18n } = useTranslation();
+    const [visibleIndex, setVisible] = useState(short ? 0 : 1);
+
+    useEffect(() => {
+        setVisible(short ? 0 : 1);
+    }, [short]);
 
     const toggle = () => {
         i18n.changeLanguage(i18n.language === 'en' ? 'ru' : 'en');
@@ -21,7 +27,12 @@ export const LangSwitcher = ({ className }: LangSwitcherProps) => {
             theme={ButtonTheme.CLEAR}
             onClick={toggle}
         >
-            {t('Язык')}
+            <span className={`${cls.animatedText} ${visibleIndex === 0 ? cls.visibleShort : ''}`}>
+                {t('Короткий язык')}
+            </span>
+            <span className={`${cls.animatedText} ${visibleIndex === 1 ? cls.visibleLong : ''}`}>
+                {t('Язык')}
+            </span>
         </Button>
     );
 };
