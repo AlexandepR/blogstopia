a3ce092b8370f94f31cb2dce76cbe686077c22e6
Alex <alexandepp@gmail.com>
2024-05-19 19:18:01 +0300

    add registration and login form in modal

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,8 +1,11 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Modal } from 'shared/ui/Modal/Modal';
-import { Loader } from 'shared/ui/Loader/Loader';
-import { Suspense } from 'react';
-import { LoginFormAsync } from 'features/AuthByUsername/ui/LoginForm/LoginForm.async';
+import { LoginForm } from '../LoginForm/LoginForm';
+import cls from './LoginModal.module.scss';
+import { useState } from 'react';
+import { RegistrationForm } from 'features/AuthByUsername/ui/RegistrationForm/RegistrationForm';
+import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
 
 export interface LoginModalProps {
     className?: string
@@ -11,17 +14,48 @@ export interface LoginModalProps {
 }
 
 export const LoginModal = (props: LoginModalProps) => {
-    const { className, isOpen, onClose } = props;
+    const [isLoginView, setIsLoginView] = useState(true);
+    const { t } = useTranslation();
+
+    const switchToLogin = () => {
+        setIsLoginView(true);
+    };
+    const switchToRegistration = () => {
+        setIsLoginView(false);
+    };
+
+    const {
+        className,
+        isOpen,
+        onClose
+    } = props;
     return (
         <Modal
-            className={classNames('', {}, [className])}
+            className={classNames(cls.LoginModal, {}, [className])}
             isOpen={isOpen}
             onClose={onClose}
             lazy
         >
-            {/* <Suspense fallback={<Loader />}> */}
-            {/*    <LoginFormAsync onSuccess={onClose} /> */}
-            {/* </Suspense> */}
+            <div>
+                <div className={cls.btnWrapper}>
+                    <Button
+                        style={isLoginView ? undefined : { fontWeight: 'bold' }}
+                        className={cls.btn}
+                        onClick={switchToRegistration}
+                    >{t('Войти')}
+                    </Button>
+                    <Button
+                        style={isLoginView ? { fontWeight: 'bold' } : undefined}
+                        className={cls.btn}
+                        onClick={switchToLogin}
+                    >{t('Регистрация')}
+                    </Button>
+                </div>
+                { isLoginView
+                    ? <RegistrationForm />
+                    : <LoginForm />
+                }
+            </div>
         </Modal>
     );
 };
