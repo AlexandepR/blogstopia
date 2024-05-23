334a8596500fa11793c0731b400518cc67d5d49a
Alex <alexandepp@gmail.com>
2024-05-23 19:59:38 +0300

    exclude ForgotPass from LoginForm

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,72 +1,44 @@
 import { useTranslation } from 'react-i18next';
-import { classNames } from 'shared/lib/classNames/className';
 import { Button } from 'shared/ui/Button/Button';
 import cls from './LoginForm.module.scss';
 import { Input } from 'shared/ui/Input/Input';
-import { useState } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
 
 interface LoginFormProps {
     className?: string
+    switchToForgotPassword: () => void
 }
 
-export const LoginForm = ({ className }: LoginFormProps) => {
+export const LoginForm = ({ className, switchToForgotPassword }: LoginFormProps) => {
     const { t } = useTranslation();
-    const [forgotPass, setForgotPass] = useState(false);
-
-    const onForgotPassword = () => {
-        setForgotPass(true);
-    };
 
     return (
         <div className={classNames(cls.LoginForm, {}, [className])}>
-            {forgotPass
-                ? <div>
-                    <div className={cls.content}>
-                        <label>{t('Почта')}</label>
-                        <Input
-                            autofocus
-                            type="text"
-                            className={cls.input}
-                        />
-                    </div>
-                    <div className={cls.btnWrapper}>
-                        <Button
-                        >{t('Отправить')}
-                        </Button>
-                        <Button
-                            className={cls.btnCancel}
-                            onClick={() => { setForgotPass(false); }}
-                        >{t('Назад')}
-                        </Button>
-                    </div>
+            <div>
+                <div className={cls.content}>
+                    <label>{t('Логин или почта')}</label>
+                    <Input
+                        autofocus
+                        type="text"
+                        className={cls.input}
+                    />
+                    <label>{t('Пароль')}</label>
+                    <Input
+                        type="password"
+                        className={cls.inputPass}
+                    />
                 </div>
-                : <div>
-                    <div className={cls.content}>
-                        <label>{t('Логин или почта')}</label>
-                        <Input
-                            autofocus
-                            type="text"
-                            className={cls.input}
-                        />
-                        <label>{t('Пароль')}</label>
-                        <Input
-                            type="password"
-                            className={cls.inputPass}
-                        />
-                    </div>
-                    <div className={cls.btnWrapper}>
-                        <Button>
-                            {t('Войти')}
-                        </Button>
-                        <Button
-                            className={cls.btnForgot}
-                            onClick={onForgotPassword}
-                        >{t('Восстановить пароль')}
-                        </Button>
-                    </div>
+                <div className={cls.btnWrapper}>
+                    <Button>
+                        {t('Войти')}
+                    </Button>
+                    <Button
+                        className={cls.btnForgot}
+                        onClick={switchToForgotPassword}
+                    >{t('Восстановить пароль')}
+                    </Button>
                 </div>
-            }
-
+            </div>
         </div>
     );
 };
