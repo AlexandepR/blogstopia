c92f5b82d3f73e533d344e1f1ebd728dee1c0dd9
Alex <alexandepp@gmail.com>
2024-06-29 11:16:45 +0300

    add registration by user name

---
+++ src/features/RegistrationByUserName/model/services/registrationByUsername/registrationByUsername.ts
@@ -0,0 +1,41 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import i18n from 'shared/config/i18n/i18n';
+
+interface RegistrationByUsernameProps {
+    login: string;
+    email: string;
+    password: string;
+}
+
+// type RegistrationResponseStatus = 'NO_CONTENT' | 'BAD_REQUEST';
+type RegistrationResponseStatus = string;
+
+export const registrationByUsername = createAsyncThunk<
+    string,
+    RegistrationByUsernameProps,
+    {
+        rejectValue: string;
+    }
+>(
+    'registrationByUsername',
+    async (
+        { login, email, password }: RegistrationByUsernameProps,
+        thunkAPI,
+    ) => {
+        try {
+            const response = await axios.post(`${__API__}/auth/registration`, {
+                login,
+                email,
+                password,
+            });
+            console.log(response, '----registrationByUsername----response');
+            return response.data;
+        } catch (err) {
+            console.log(err);
+            return thunkAPI.rejectWithValue(
+                i18n.t('Логин или Почта уже существует'),
+            );
+        }
+    },
+);
