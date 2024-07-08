7bb43a96ab5c4d0be5a556904f8d5e7446321fad
Alex <alexandepp@gmail.com>
2024-07-08 22:17:12 +0300

    refactor LoginForm

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,8 +1,13 @@
 import { useTranslation } from 'react-i18next';
-import { Button } from 'shared/ui/Button/Button';
+import { Button, ButtonSize, Hover } from 'shared/ui/Button/Button';
 import cls from './LoginForm.module.scss';
 import { Input } from 'shared/ui/Input/Input';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loginActions } from '../../model/slice/loginSlice';
+import { getLoginState } from '../../model/selectors/getLoginState';
+import { loginByUserName } from 'features/AuthByUsername/model/services/loginByUserName';
 
 interface LoginFormProps {
     className?: string;
@@ -14,20 +19,52 @@ export const LoginForm = ({
     switchToForgotPassword,
 }: LoginFormProps) => {
     const { t } = useTranslation();
+    const dispatch = useDispatch<any>();
+    const { loginOrEmail, password, error } = useSelector(getLoginState);
+
+    const onChangeLogin = useCallback(
+        (value: string) => {
+            dispatch(loginActions.setLoginOrEmail(value));
+        },
+        [dispatch],
+    );
+
+    const onChangePassword = useCallback(
+        (value: string) => {
+            dispatch(loginActions.setPassword(value));
+        },
+        [dispatch],
+    );
+
+    const onLoginClick = useCallback(() => {
+        dispatch(loginByUserName({ loginOrEmail, password }));
+    }, [dispatch, loginOrEmail, password]);
 
     return (
         <div className={classNames(cls.LoginForm, {}, [className])}>
             <div>
                 <div className={cls.content}>
                     <label>{t('Логин или почта')}</label>
-                    <Input autofocus type="text" className={cls.input} />
+                    <Input
+                        autofocus
+                        type="text"
+                        className={cls.input}
+                        value={loginOrEmail}
+                        onChange={onChangeLogin}
+                    />
                     <label>{t('Пароль')}</label>
-                    <Input type="password" className={cls.inputPass} />
+                    <Input
+                        type="password"
+                        className={cls.inputPass}
+                        value={password}
+                        onChange={onChangePassword}
+                    />
                 </div>
                 <div className={cls.btnWrapper}>
-                    <Button>{t('Войти')}</Button>
+                    <Button onClick={onLoginClick}>{t('Войти')}</Button>
                     <Button
-                        className={cls.btnForgot}
+                        hover={Hover.UNDERLINE}
+                        size={ButtonSize.SM}
                         onClick={switchToForgotPassword}
                     >
                         {t('Восстановить пароль')}
