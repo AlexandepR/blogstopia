bcd23572cc8c8c058f023608f9f10d76e9c5e851
Alex <alexandepp@gmail.com>
2024-08-16 23:55:33 +0300

    add test for getLoginError

---
+++ src/features/AuthByUsername/model/selectors/getLoginError/getLoginError.test.ts
@@ -0,0 +1,26 @@
+import { getLoginError } from 'features/AuthByUsername/model/selectors/getLoginError/getLoginError';
+import type { StateSchema } from 'app/providers/StoreProvider';
+import type { DeepPartial } from 'app/providers/StoreProvider/config/StateSchema';
+
+describe('getLoginError', () => {
+    test('should return error', () => {
+        const state: DeepPartial<StateSchema> = {
+            login: {
+                loginOrEmail: '',
+                password: '',
+                isLoading: false,
+                error: 'Неверный логин или пароль',
+            },
+        };
+
+        expect(getLoginError(state as StateSchema)).toEqual(
+            'Неверный логин или пароль',
+        );
+    });
+
+    test('should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {};
+
+        expect(getLoginError(state as StateSchema)).toEqual(undefined);
+    });
+});
