d18821d648b76bd7b70e806f545f0a7f61e7c1bd
Alex <alexandepp@gmail.com>
2024-09-16 20:27:49 +0300

    refactor thunk confirm by code, use extra.api

--- src/features/ConfirmByCode/model/services/confirmByCode.ts
+++ src/features/ConfirmByCode/model/services/confirmByCode.ts
@@ -1,8 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
 import i18n from 'shared/config/i18n/i18n';
+import type { ThunkConfig } from 'app/providers/StoreProvider';
 
-type ConfirmResponseStatus = string;
 interface ConfirmCode {
     code: string;
 }
@@ -10,18 +9,18 @@ interface ConfirmCode {
 export const confirmByCode = createAsyncThunk<
     string,
     ConfirmCode,
-    { rejectValue: string }
+    ThunkConfig<string>
 >('confirmCode', async ({ code }: ConfirmCode, thunkAPI) => {
+    const { extra, rejectWithValue } = thunkAPI;
     try {
-        console.log('-----', code, '--------code-----');
-        const response = await axios.post(
-            `${__API__}/auth/registration-confirmation`,
+        const response = await extra.api.post(
+            `/auth/registration-confirmation`,
             {
                 code,
             },
         );
         return response.data;
     } catch (e) {
-        return thunkAPI.rejectWithValue(i18n.t('Неверный код подтверждения'));
+        return rejectWithValue(i18n.t('Неверный код подтверждения'));
     }
 });
